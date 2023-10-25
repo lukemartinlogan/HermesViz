1086ae8771ab6cc0e6caf8605824f81b2ee921e6
Jaime Cernuda <jcernudagarcia@hawk.iit.edu>
2023-10-24 21:01:58 -0500

    addapting the new metadata on the js

--- static/js/sketch_231013c.js
+++ static/js/sketch_231013c.js
@@ -10,38 +10,54 @@ let bottom_margin;
 let data;
 let prev_data = {};
 let loadingData = false;
+
+let dropdown;
+let DELTA_THRESHOLD = 0.05;
+let userSetNodes = false;
+
+let isPlaying = true;
+let playPauseButton;
+
 function setup() {
-  adjustVariables();
-  createCanvas(WINDOW_WIDTH, WINDOW_HEIGHT);
-  frameRate(1);
-  background(200);
-
-  dropdown = createSelect();
-  dropdown.position(10, 10);
-  dropdown.option('4');
-  dropdown.option('8');
-  dropdown.option('16');
-  dropdown.option('32');
-  dropdown.selected('32');  // Default value
-  dropdown.changed(updateNodes);
-}
-
-function updateNodes() {
-    NUM_NODES = int(dropdown.value());  // Update the number of nodes based on the dropdown value
-    adjustVariables();
-    redraw();  // Redraw the heatmap with the new number of nodes
-}
-
-function adjustVariables() { 
     WINDOW_WIDTH = windowWidth;
     WINDOW_HEIGHT = windowHeight;
 
-    left_margin = WINDOW_WIDTH * 0.05;  // 5% of window width
-    right_margin = WINDOW_WIDTH * 0.05;  // 5% of window width
-    bottom_margin = WINDOW_HEIGHT * 0.1;  // 10% of window height
+    createCanvas(WINDOW_WIDTH, WINDOW_HEIGHT);
+    frameRate(1);
+    background(200);
+
+    dropdown = createSelect();
+    dropdown.position(10, 10);
+    updateDropdownOptions(NUM_NODES, NUM_NODES);  // Initialize dropdown with current NUM_NODES
+    dropdown.changed(updateNodes);
+
+    playPauseButton = createButton('Pause');
+    playPauseButton.position(dropdown.x + dropdown.width + 20, 10);
+    playPauseButton.mousePressed(togglePlayPause);
+}
+
+function togglePlayPause() {
+    isPlaying = !isPlaying;
+    playPauseButton.html(isPlaying ? 'Pause' : 'Play');
+}
+
+function updateNodes() {
+    NUM_NODES = int(dropdown.value());
+    userSetNodes = true; // User has manually set the number of nodes
+    adjustVariables();
+    redraw();
+}
+
+function adjustVariables() {
+    WINDOW_WIDTH = windowWidth;
+    WINDOW_HEIGHT = windowHeight;
+
+    left_margin = WINDOW_WIDTH * 0.05;
+    right_margin = WINDOW_WIDTH * 0.05;
+    bottom_margin = WINDOW_HEIGHT * 0.1;
 
     cell_width = (WINDOW_WIDTH - left_margin - right_margin) / NUM_NODES;
-    cell_height = (WINDOW_HEIGHT - bottom_margin) * 0.3 / 4;  // 60% of the available height divided by 4 rows
+    cell_height = (WINDOW_HEIGHT - bottom_margin) * 0.3 / 4;
 }
 
 function get_cell_color(value) {
@@ -57,46 +73,57 @@ function draw_gradient_bar(x, y, w, h) {
     }
 }
 
-function draw_row_labels(start_y) {
+function draw_row_labels(start_y, labels) {
     fill(0);
     noStroke();
-    let labels = ["Memory", "Nvme", "BB", "Pfs"];
     for (let i = 0; i < labels.length; i++) {
         text(labels[i], 10, start_y + i * cell_height + cell_height / 2);
     }
 }
 
-function draw_arrow(x, y, direction) {
-    let arrow_size = cell_width / 8;  // Calculate arrow size based on cell width
-    let max_arrow_size = 10;  // Set a maximum size for the arrow
+function draw_arrow(x, y, change) {
+    let arrow_size = cell_width / 10;  // Start with a base size
+    let scaling_factor = constrain(map(NUM_NODES, 1, 32, 1.5, 1), 1, 1.5);  // Scale based on number of nodes
+    arrow_size *= scaling_factor;
+
+    let max_arrow_size = 15;  // Set a maximum size for the arrow
     arrow_size = min(arrow_size, max_arrow_size);  // Use the smaller of the two values
 
     push();  // Save the current drawing style
-    noStroke();  // Remove the stroke for the arrows
 
-    if (direction === "up") {
-        fill(255, 255, 0);  // Yellow for increase
-        triangle(x, y - arrow_size,
-                 x - arrow_size, y + arrow_size,
-                 x + arrow_size, y + arrow_size);
-    } else if (direction === "down") {
-        fill(0, 0, 255);  // Blue for decrease
-        triangle(x, y + arrow_size,
-                 x - arrow_size, y - arrow_size,
-                 x + arrow_size, y - arrow_size);
+    if (Math.abs(change) < DELTA_THRESHOLD) {
+        fill(255, 255, 255);  // White color for negligible change
+        textSize(arrow_size * 5);  // Increase text size
+        textAlign(CENTER, CENTER);
+        text("=", x, y);
+    } else {
+        let direction = change > 0 ? "up" : "down";
+        if (direction === "up") {
+            fill(255, 255, 0);  // Yellow for increase
+            noStroke();
+            triangle(x, y - arrow_size,
+                x - arrow_size, y + arrow_size,
+                x + arrow_size, y + arrow_size);
+        } else if (direction === "down") {
+            fill(0, 0, 255);  // Blue for decrease
+            noStroke();
+            triangle(x, y + arrow_size,
+                x - arrow_size, y - arrow_size,
+                x + arrow_size, y - arrow_size);
+        }
     }
 
     pop();  // Restore the previous drawing style
 }
 
 function generate_heatmap(data) {
-    let heatmap_height = 4 * cell_height;
+    let nodeNames = Object.keys(data);
+    let targetTypes = Object.keys(data[nodeNames[0]]);  // Assuming all nodes have the same target types
+    let heatmap_height = targetTypes.length * cell_height;
+
     let start_y = height - bottom_margin - heatmap_height;
 
-    draw_row_labels(start_y);  // Draw the row labels
-
-    // Get the list of node names
-    let nodeNames = Object.keys(data);
+    draw_row_labels(start_y, targetTypes);
 
     // Loop over the top NUM_NODES nodes
     for (let i = 0; i < NUM_NODES; i++) {
@@ -104,7 +131,7 @@ function generate_heatmap(data) {
 
         // Draw column labels
         fill(0);  // Set text color to black
-        text("Node " + (parseInt(col.split('-')[2]) + 1), left_margin + i * cell_width + cell_width / 2 - textWidth("Node " + (parseInt(col.split('-')[2]) + 1)) / 2, start_y - 5);
+        text("Node " + (i + 1), left_margin + i * cell_width + cell_width / 2 - textWidth("Node " + (i + 1)) / 2, start_y - 5);
 
         let nodeData = data[col];
         for (let key in nodeData) {
@@ -116,10 +143,13 @@ function generate_heatmap(data) {
             let arrow_x = left_margin + i * cell_width + cell_width / 2;
             let arrow_y = start_y + Object.keys(nodeData).indexOf(key) * cell_height + cell_height / 2;
             if (prev_data[col] && prev_data[col][key] !== undefined) {
-                if (value > prev_data[col][key]) {
-                    draw_arrow(arrow_x, arrow_y, "up");
-                } else if (value < prev_data[col][key]) {
-                    draw_arrow(arrow_x, arrow_y, "down");
+                let change = value - prev_data[col][key];
+                if (change > 0) {
+                    draw_arrow(arrow_x, arrow_y, change);
+                } else if (change < 0) {
+                    draw_arrow(arrow_x, arrow_y, change);
+                } else {
+                    draw_arrow(arrow_x, arrow_y, change);
                 }
             }
 
@@ -130,12 +160,12 @@ function generate_heatmap(data) {
     }
 
     // Draw the gradient bar below the heatmap
-    let gradient_bar_y = start_y + heatmap_height + 10;  // 10 units below the heatmap
+    let gradient_bar_y = start_y + heatmap_height + 10;  // Adjusted to use heatmap_height
     let gradient_bar_width = (WINDOW_WIDTH - left_margin - right_margin) * 0.6;  // Reduced length of the gradient bar
     let gradient_bar_height = 15;  // Reduced height of the gradient bar
     let gradient_bar_x = (WINDOW_WIDTH - gradient_bar_width) / 2;  // Center the gradient bar
     draw_gradient_bar(gradient_bar_x, gradient_bar_y, gradient_bar_width, gradient_bar_height);
-    
+
     // Label the gradient bar
     fill(0);  // Set text color to black
     noStroke();
@@ -144,25 +174,64 @@ function generate_heatmap(data) {
 }
 
 function draw() {
-  background(255);
+    background(255);
 
-  if (!loadingData) {
-    loadingData = true;
-    httpGet('/data', 'json', false, function(response) {
-      data = response;
-      loadingData = false;
-    }, function(error) {
-      console.error(error);
-      loadingData = false;
-    });
-  }
+    if (isPlaying) {
+        if (!loadingData) {
+            loadingData = true;
+            httpGet('/metadata', 'json', false, function (response) {
+                data = preprocessData(response);
+                loadingData = false;
+            }, function (error) {
+                console.error(error);
+                loadingData = false;
+            });
+        }
+    }
 
-  if (data) {
-    console.log('Data received:', data);
-    text("Window Width is " + windowWidth, 30, 40);
-    generate_heatmap(data);
-    prev_data = data;
-  }
+    if (data) {
+        fill(0);  // Set text color to black
+        console.log('Data received:', data);
+        // text("Window Width is " + windowWidth, 30, 40);
+        generate_heatmap(data);
+        if (isPlaying) {
+            prev_data = JSON.parse(JSON.stringify(data));
+        }
+    }
+}
+
+function preprocessData(metadata) {
+    const nodes = {};
+    let numTargets = 0;
+
+    for (const target of metadata.targets) {
+        const node_id = target.node_id;
+        const target_type = target.name;
+        const remaining_capacity = target.remaining_capacity;
+
+        if (!(node_id in nodes)) {
+            nodes[node_id] = {};
+        }
+
+        nodes[node_id][target_type] = remaining_capacity;
+        numTargets = Math.max(numTargets, Object.keys(nodes[node_id]).length);
+    }
+
+    if (!userSetNodes) { // Only update if the user hasn't set a value
+        NUM_NODES = Object.keys(nodes).length;
+        updateDropdownOptions(NUM_NODES, NUM_NODES);
+    }
+
+    adjustVariables();
+    return nodes;
+}
+
+function updateDropdownOptions(numNodes, selectedValue) {
+    dropdown.html('');  // Clear existing options
+    for (let i = 1; i <= numNodes; i++) {
+        dropdown.option(i.toString());
+    }
+    dropdown.selected(selectedValue.toString());  // Set default selection based on parameter
 }
 
 function windowResized() {
